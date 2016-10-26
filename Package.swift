3c5ae5b1c966075f676eaf61044d20f3cf9d356a
Jaanus Siim <jaanus@jaanussiim.com>
2016-10-26 20:08:45 +0300

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "CloudCopy"
+    name: "CloudCopy",
+    dependencies: [
+        .Package(url: "https://github.com/coodly/swlogger.git", Version(0, 3, 1)),
+        .Package(url: "https://github.com/coodly/TalkToCloud.git", Version(0, 4, 0)),
+    ]
 )
